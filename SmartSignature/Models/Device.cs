e00d22d297252593c7971c7693d5b71be38886b7
Amanda Hagy <amandahagy@hotmail.com>
2020-04-07 20:33:46 -0300

    Changing data type for device name

--- SmartSignature/Models/Device.cs
+++ SmartSignature/Models/Device.cs
@@ -5,7 +5,7 @@ namespace SmartSignature.Models
     public class Device
     {
         int DeviceId { get; set; }
-        int DeviceName { get; set; }
+        string DeviceName { get; set; }
         int ActivationCode { get; set; }
         DateTime ActivationDate { get; set; }
         User CaixaAccount { get; set; }
