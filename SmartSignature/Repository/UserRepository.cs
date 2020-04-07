a9a7ea2d4c76f8365c3d173442ff5ee1a8f2c12a
Amanda Hagy <amandahagy@hotmail.com>
2020-04-07 15:58:45 -0300

    User operations

---
+++ SmartSignature/Repository/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using SmartSignature.Models;
+using SmartSignature.Repository.Context;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmartSignature.Repository
+{
+    public class UserRepository
+    {
+        private readonly DataBaseContext context;
+
+        public UserRepository()
+        {
+            context = new DataBaseContext();
+        }
+
+
+        public IList<User> List()
+        {
+            return context.User.ToList();
+        }
+
+
+        public User Find(int id)
+        {
+            return context.User.Find(id);
+        }
+
+        public void Add(User user)
+        {
+            context.User.Add(user);
+            context.SaveChanges();
+        }
+
+        public void Update(User user)
+        {
+            context.User.Update(user);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var user = new User()
+            {
+                CaixaAccount = id
+            };
+
+            context.User.Remove(user);
+            context.SaveChanges();
+        }
+
+    }
+}
