0ffc5e4d95e4cff167f2f995df2a4f9f9699cb6a
Amanda Hagy <amandahagy@hotmail.com>
2020-04-07 14:49:33 -0300

    Creating database context class

---
+++ SmartSignature/Repository/Context/DataBaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using SmartSignature.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Oracle.EntityFrameworkCore;
+
+namespace SmartSignature.Repository.Context
+{
+    public class DataBaseContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+                optionsBuilder.UseOracle(config.GetConnectionString("FiapSmartCityConnection"));
+            }
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+    }
+}
