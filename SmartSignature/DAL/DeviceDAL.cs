cc89b77c48b20af448eb8976d2e1a2f6563bdccc
Amanda Hagy <amandahagy@hotmail.com>
2020-04-07 21:37:03 -0300

    Adding DeviceDAL

---
+++ SmartSignature/DAL/DeviceDAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SmartSignature.Models;
+using SmartSignature.Repository.Context;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace SmartSignature.DAL
+{
+    public class DeviceDAL
+    {
+        public DeviceDAL()
+        {
+        }
+
+        public Device Find(int id)
+        {
+            DataBaseContext context = new DataBaseContext();
+            var dev = context.Device.Include(t => t.Account)
+                .FirstOrDefault(p => p.DeviceId == id);
+            return dev;
+        }
+    }
+}
